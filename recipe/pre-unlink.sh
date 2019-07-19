be54b1b6936663550b8b9c7d7ff6095d1ab1c883
Eric Dill <thedizzle@gmail.com>
2019-07-19 17:13:33 -0400

    making progress, but still stuck

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,6 +1,7 @@
-"${PREFIX}/bin/jupyter-serverextension" disable sparkmagic --py --sys-prefix > /dev/null 2>&1
-
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f sparkkernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f pysparkkernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f pyspark3kernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f sparkrkernel
+{
+    ${PREFIX}/bin/jupyter-serverextension  disable sparkmagic --py --sys-prefix
+    ${PREFIX}/bin/jupyter-kernelspec uninstall $SP_DIR/sparkmagic/kernels/sparkkernel --sys-prefix
+    ${PREFIX}/bin/jupyter-kernelspec uninstall $SP_DIR/sparkmagic/kernels/pysparkkernel --sys-prefix
+    ${PREFIX}/bin/jupyter-kernelspec uninstall $SP_DIR/sparkmagic/kernels/pyspark3kernel --sys-prefix
+    ${PREFIX}/bin/jupyter-kernelspec uninstall $SP_DIR/sparkmagic/kernels/sparkrkernel --sys-prefix
+} >> "${PREFIX}/.messages.txt" 2>&1
