98a933ba307d95b511b658176e02bfab46b18069
Eric Dill <thedizzle@gmail.com>
2019-07-19 20:42:00 +0000

    Update sparkmagic to deal with linking scripts properly

--- recipe/sparkmagic.sh
+++ recipe/sparkmagic.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 cd sparkmagic && python -m pip install . -vv --no-deps
+
+POST_LINK="$PREFIX"/bin/.sparkmagic-post-link.sh
+PRE_UNLINK="$PREFIX"/bin/.sparkmagic-pre-unlink.sh
+
+mkdir -p ${PREFIX}/bin
+
+cp ${RECIPE_DIR}/post-link.sh "$POST_LINK"
+cp ${RECIPE_DIR}/pre-unlink.sh "$PRE_UNLINK"
+
+chmod +x "$POST_LINK" "$PRE_UNLINK"
